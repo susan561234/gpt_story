8b1c31e67e0ab12fb058b6c86fec70b6a3a29bc0
you shuhan <45191972+susan561234@users.noreply.github.com>
2023-05-25 09:42:04 +0800

    函式修改

--- app1.py
+++ app1.py
@@ -13,10 +13,83 @@ import uuid
 from dash import html
 from dash import dcc
 from openai.error import RateLimitError
+
+def story_split_start(story):
+    if "故事名稱:" in story:
+        story_split =  story.split("故事名稱:")
+        story_split_1 = story_split[1].split("故事開頭:")
+    elif "故事名稱：" in story:
+        story_split =  story.split("故事名稱：")
+        story_split_1 = story_split[1].split("故事開頭：")
+    else:
+        pass
+    main_character = story_split[0]
+    story_name = "<<" + story_split_1[0] + ">>"
+    if "選項:1." in story:
+        button = story.split("選項:1.")[1]
+        story_notbutton = story_split_1[1].split("選項:1.")[0]
+    elif "選項: 1." in story:
+        button = story.split("選項: 1.")[1]
+        story_notbutton = story_split_1[1].split("選項: 1.")[0]
+    else:
+        button = story.split("選項：1.")[1]
+        story_notbutton = story_split_1[1].split("選項：1.")[0]
+    button1 = button.split("2.")[0]
+    button3 = button.split("3.")[1]  
+    button2 = button[len(button1)+2:len(button)-len(button3)-2]
+    return main_character, story_name,story_notbutton,button1,button2,button3
+def story_split_item(updated_data):
+    if "選項:1." in updated_data:
+        button = updated_data.split("選項:1.")[1]
+        story_notbutton = updated_data.split("選項:1.")[0][5:]
+    elif "選項: 1." in updated_data:
+        button = updated_data.split("選項: 1.")[1]
+        story_notbutton = updated_data.split("選項: 1.")[0][5:]
+    else:
+        button = updated_data.split("選項：1.")[1]
+        story_notbutton = updated_data.split("選項：1.")[0][5:]
+    try:
+        other_word = story_notbutton.split("故事內容")[0]
+        if len(story_notbutton) != len(other_word):
+            story_notbutton = story_notbutton.split("故事內容")[1][1:]
+    except IndexError as e:
+        print("IndexError",e)
+        story_notbutton = story_notbutton.split("故事內容")[0]
+    button1 = button.split("2.")[0]
+    button3 = button.split("3.")[1]  
+    button2 = button[len(button1)+2:len(button)-len(button3)-2]
+    return story_notbutton,button1,button2,button3
+def story_split_over(updated_data):
+    updated_data = updated_data[5:]
+    if "。" in updated_data:
+        count = updated_data.split("。")
+        if len(count) == 3:
+            button_1 = count[0]
+            button_2 = count[1]
+            button_3 = count[2]
+        elif  len(count) >3:
+            button_1 = count[0]+count[1]
+            button_2 = count[len(count)-2]
+            button_3 = count[len(count)-1]
+        elif len(count) <3:
+            if len(count) == 2:
+                button_1 = count[0]+count[1]
+                button_2 = count[0]+count[1]
+                button_3 = count[1]
+            else :
+                button_1 = count[0]+count[0]
+                button_2 = count[0]+count[0]
+                button_3 = count[0]
+    return updated_data,button_1,button_2,button_3
+
 def process_image_and_voice(main_character,story_notbutton,buttom1,buttom2,buttom3):
-    #image_url = Image.Image1(story_notbutton)
     image_url = Image.stable_diffusion_photo(main_character,story_notbutton,buttom1,buttom2,buttom3)
     voice.TTS(story_notbutton)
+    return image_url
+def image_voice(main_character,story_notbutton,button1,button2,button3):
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(process_image_and_voice, main_character,story_notbutton,button1,button2,button3)
+        image_url = future.result()
     return image_url
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -132,7 +205,6 @@ def index():
         response.set_cookie('user_id', user_id)
     else:
         response = make_response()
-    
     option_count = 0
     ip = request.remote_addr
     start_time = datetime.datetime.now()
@@ -146,40 +218,14 @@ def start():
     global story_name, imgurl, content, image_url, main_character, style
     try:
         story = story_generator.generate_story_beginning(style)
+        main_character, story_name,story_notbutton,button1,button2,button3 = story_split_start(story)
     except Exception as e:
         print(f"An error occurred: {e}")
-
         return redirect(url_for("index"))
-    if "故事名稱:" in story:
-        story_split =  story.split("故事名稱:")
-        story_split_1 = story_split[1].split("故事開頭:")
-    elif "故事名稱：" in story:
-        story_split =  story.split("故事名稱：")
-        story_split_1 = story_split[1].split("故事開頭：")
-    else:
-        pass
-    main_character = story_split[0]
-
-    story_name = "<<" + story_split_1[0] + ">>"
-    if "選項:1." in story:
-        button = story.split("選項:1.")[1]
-        story_notbutton = story_split_1[1].split("選項:1.")[0]
-    elif "選項: 1." in story:
-        button = story.split("選項: 1.")[1]
-        story_notbutton = story_split_1[1].split("選項: 1.")[0]
-    else:
-        button = story.split("選項：1.")[1]
-        story_notbutton = story_split_1[1].split("選項：1.")[0]
+    
     content += story_notbutton
-
-    button1 = button.split("2.")[0]
-    button3 = button.split("3.")[1]  
-    button2 = button[len(button1)+2:len(button)-len(button3)-2]
-
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(process_image_and_voice, main_character,story_notbutton,button1,button2,button3)
-        image_url = future.result()
-        imgurl["first"] = image_url
+    image_url = image_voice(main_character,story_notbutton,button1,button2,button3)
+    imgurl["first"] = image_url
     image_url1 = image_url[0] 
     return render_template('test1.html',story=story_notbutton,button1=button1,button2=button2,button3=button3,image_url=image_url1,story_name=story_name,option_count=option_count)
 
@@ -187,7 +233,7 @@ def start():
 def update_story():
     global option_count, content, image_url, main_character
     button_value = request.form.get('buttonValue')
-    story_notbutton = request.form.get('storyNotButton')  
+    content += button_value#上次所選的選項
     try:
         updated_data, option_count = story_generator.generate_story_item(button_value, option_count)
     except Exception as e:
@@ -197,72 +243,35 @@ def update_story():
         }
         return jsonify(response)
     print(updated_data, option_count)
-    updated_data = updated_data[5:]
-    if option_count == 4 :
+    if option_count == 4:
+        updated_data,button_1,button_2,button_3 = story_split_over(updated_data)
         content += updated_data
-        if "。" in updated_data:
-            count = updated_data.split("。")
-            if len(count) == 3:
-                button_1 = count[0]
-                button_2 = count[1]
-                button_3 = count[2]
-            elif  len(count) >3:
-                button_1 = count[0]+count[1]
-                button_2 = count[len(count)-2]
-                button_3 = count[len(count)-1]
-            elif len(count) <3:
-                if len(count) == 2:
-                    button_1 = count[0]+count[1]
-                    button_2 = count[0]+count[1]
-                    button_3 = count[1]
-                else :
-                    button_1 = count[0]+count[0]
-                    button_2 = count[0]+count[0]
-                    button_3 = count[0]
-        with ThreadPoolExecutor() as executor:
-            future = executor.submit(process_image_and_voice, main_character,story_notbutton,button_1,button_2,button_3)
-            image_url = future.result()
-            print(image_url)
-            imgurl["final"] = image_url
-        DB.DB_over(ip,start_time,datetime.datetime.now(),datetime.datetime.combine(datetime.datetime.now().date(), datetime.datetime.min.time()),style,story_name,content,imgurl)
+        image_url = image_voice(main_character,updated_data,button_1,button_2,button_3)
+        imgurl["final"] = image_url
         image_url1 = image_url[0]
         response = {
             'story': updated_data,
             'image_url': image_url1,
-            'option_count': str(option_count),
             'redirect': option_count
         }
+        DB.DB_over(ip,start_time,datetime.datetime.now(),datetime.datetime.combine(datetime.datetime.now().date(), datetime.datetime.min.time()),style,story_name,content,imgurl)
         return jsonify(response)
-    if "選項:1." in updated_data:
-        button = updated_data.split("選項:1.")[1]
-        story_notbutton = updated_data.split("選項:1.")[0][5:]
-    elif "選項: 1." in updated_data:
-        button = updated_data.split("選項: 1.")[1]
-        story_notbutton = updated_data.split("選項: 1.")[0][5:]
-    else:
-        button = updated_data.split("選項：1.")[1]
-        story_notbutton = updated_data.split("選項：1.")[0][5:]
     try:
-        other_word = story_notbutton.split("故事內容")[0]
-        if len(story_notbutton) != len(other_word):
-            story_notbutton = story_notbutton.split("故事內容")[1][1:]
-    except IndexError as e:
-        print("IndexError",e)
-        story_notbutton = story_notbutton.split("故事內容")[0]
-    button1 = button.split("2.")[0]
-    button3 = button.split("3.")[1]  
-    button2 = button[len(button1)+2:len(button)-len(button3)-2]
+        story_notbutton,button1,button2,button3 = story_split_item(updated_data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        response = {
+            'redirect': 404
+        }
+        return jsonify(response)
     
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(process_image_and_voice, main_character,story_notbutton,button1,button2,button3)
-        image_url = future.result()
-        if option_count==1:
-            imgurl["second"] = image_url
-        elif option_count==2:
-            imgurl["third"] = image_url
-        else:
-            imgurl["fourth"] = image_url
-    content += button_value
+    image_url = image_voice(main_character,story_notbutton,button1,button2,button3)
+    if option_count==1:
+        imgurl["second"] = image_url
+    elif option_count==2:
+        imgurl["third"] = image_url
+    else:
+        imgurl["fourth"] = image_url
     content += story_notbutton
     image_url1 = image_url[0]
     response = {
